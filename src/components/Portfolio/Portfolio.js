174292cc2305f3389a35e75470921b1d1daf7628
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-10-31 14:36:09 +0800

    portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,9 +1,64 @@
+import { useState, useEffect } from 'react'
 import './Portfolio.scss'
+import PortfolioList from '../PortfolioList/PortfolioList'
+import { portfolioLayout, portfolioReact, portfolioVue } from '../../data'
 
 export default function Portfolio() {
-    return (
-        <div className="portfolio" id="portfolio">
-            
-        </div>
-    )
+  const [selected, setSelected] = useState('Layout')
+  const [data, setData] = useState([])
+  const list = [
+    {
+      id: 'Layout',
+      title: '切版練習',
+    },
+    {
+      id: 'React',
+      title: 'React',
+    },
+    {
+      id: 'Vue',
+      title: 'Vue',
+    },
+  ]
+
+  useEffect(() => {
+    switch (selected) {
+      case 'Layout':
+        setData(portfolioLayout)
+        break
+      case 'React':
+        setData(portfolioReact)
+        break
+      case 'Vue':
+        setData(portfolioVue)
+        break
+      default:
+        setData(portfolioLayout)
+        break
+    }
+  }, [selected])
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        {data.map((d) => (
+          <a href={d.link} className="item">
+            <img src={d.img} alt="" />
+            <h3>{d.title}</h3>
+          </a>
+        ))}
+      </div>
+    </div>
+  )
 }
---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const portfolioLayout = [
+    {
+        id: 1,
+        title: 'KieKa RWD Website',
+        img: 'assets/Portfolio/kieka.PNG',
+        link: 'https://weichieh123.github.io/html-css-bootstrap/kieka/index.html'
+    },
+    {
+        id: 2,
+        title: 'Verse RWD Website',
+        img: 'assets/Portfolio/verse.PNG',
+        link: 'https://weichieh123.github.io/html-css-bootstrap/verse/index.html'
+    },
+    {
+        id: 3,
+        title: 'FransHals Desktop Website',
+        img: 'assets/Portfolio/FransHals.PNG',
+        link: 'https://weichieh123.github.io/html-css-bootstrap/FransHals/index.html'
+    },
+]
+
+export const portfolioReact = [
+    {
+        id: 1,
+        title: '資策會專題 - Lunar Phase',
+        img: 'assets/Portfolio/React-LunarPhase.PNG',
+        link: 'https://github.com/weichieh123/Lunar_Phase_React'
+    },
+    {
+        id: 2,
+        title: 'Todo App',
+        img: 'assets/Portfolio/React-TodoApp.PNG',
+        link: 'https://github.com/weichieh123/React-TodoApp'
+    },
+    {
+        id: 3,
+        title: 'React useContext',
+        img: 'assets/Portfolio/React-useContext.PNG',
+        link: 'https://github.com/weichieh123/learn-react-useContext'
+    },
+    {
+        id: 4,
+        title: 'React Redux',
+        img: 'assets/Portfolio/React-Redux.PNG',
+        link: 'https://github.com/weichieh123/learn-redux'
+    },
+    {
+        id: 5,
+        title: 'React Redux Toolkit',
+        img: 'assets/Portfolio/React-ReduxToolkit.PNG',
+        link: 'https://github.com/weichieh123/learn-redux-toolkit'
+    },
+]
+
+export const portfolioVue = [
+    {
+        id: 1,
+        title: 'Todo App',
+        img: 'assets/Portfolio/Vue-TodoApp.PNG',
+        link: 'https://github.com/weichieh123/Vue-TodoApp'
+    },
+]
