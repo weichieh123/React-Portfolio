66b0576ddaf36610a141746adeeba5d52c63e7d6
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-11-18 15:29:15 +0800

    修改標題

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -10,7 +10,7 @@ export default function Intro() {
         showCursor: true,
         backDelay: 1500,
         backSpeed:60,
-      strings: ['Front-end Web Engineer'],
+      strings: ['Front-end Web Developer'],
     })
   }, [])
   return (
